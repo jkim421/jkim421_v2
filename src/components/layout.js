63316289867d958a16e71b5b8568168a88b77e00
john.kim <jkim21@pm.me>
2019-01-17 00:56:51 -0500

    Create component files

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,15 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import SEO from '../components/seo'
+import './layout.css';
 
-import Header from './header'
-import './layout.css'
+const Layout = () => (
+  <>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <div>
+      Home
+    </div>
+  </>
+);
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
+export default Layout;
